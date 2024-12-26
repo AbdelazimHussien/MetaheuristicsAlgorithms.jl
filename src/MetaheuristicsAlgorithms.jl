d736c015928276580e6e9424fc30ad06725982c8
abdelazimhussien <abdelazim.hussien@liu.se>
2024-12-26 12:53:01 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.52

---
+++ src/MetaheuristicsAlgorithms.jl
@@ -0,0 +1,5 @@
+module MetaheuristicsAlgorithms
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MetaheuristicsAlgorithms
+using Test
+
+@testset "MetaheuristicsAlgorithms.jl" begin
+    # Write your tests here.
+end
